5853de09451b16c49f723b9f183c5821056916d9
rahele <rahelemomenyfar0113@gmail.com>
2024-07-09 13:54:24 +0330

    group

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AllCourseViewest
+from .views import AllCourseViewest,AllGroupViewset
 
 
 urlpatterns = [
-    path('course/',AllCourseViewest.as_view(), name = 'course')
+    path('course/',AllCourseViewest.as_view(), name = 'course'),
+    path('group/',AllGroupViewset.as_view(), name = 'group')
+
 ]
--- course/views.py
+++ course/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CourseSerializer
+from.serializers import CourseSerializer, groupingserializer
 from . import models
 import pandas as pd
 from authentication import fun
@@ -10,23 +10,22 @@ from authentication import fun
 
 class AllCourseViewest(APIView):
     def get(self,request):
-    #    token = request.headers['Authorization']
-    #    user = fun.decryptionUser(token)
-    #    if not user:
-    #         result = {'message':'کاربر یافت نشد'}
-    #         return Response(result,status=status.HTTP_400_BAD_REQUEST)
-    #    course = models.Course.objects.all()
-    #    course =CourseSerializer(course,many=True, null = True , blank = True ).data
-    #     # result = {'message':'OK'}
-    #    return Response(course,status=status.HTTP_200_OK)
+       course = models.Course.objects.all()
+       course =CourseSerializer(course,many=True, null = True , blank = True ).data
+       return Response(course,status=status.HTTP_200_OK)
 
-        token = request.header['Authorization']
-        user = fun.decryptionUser(token)
-
-        if not user:
-            result = {'gfh'}
-            return res
-
-
-
-
+class AllGroupViewset(APIView):
+    def get(self,request):
+       token = request.headers['Authorization']
+       user = fun.decryptionUser(token)
+       if not user:
+           result = {'message':'کاربر یافت نشد'}
+           return Response(result,status=status.HTTP_400_BAD_REQUEST)
+       group = models.Grouping.objects.all()
+       
+       if not group.exists():
+           return Response(request,status=status.HTTP_404_NOT_FOUND)
+       
+       serialize = groupingserializer(group,many=True)
+       return Response(serialize.data, status=status.HTTP_200_OK)
+    
